7505456718cea2f6bf340a6f7afffdac8f1c614f
Romain666667 <manmanlon90@gmail.com>
2025-10-01 08:56:38 +0200

    Premier commit

---
+++ assets/js/script.js
@@ -0,0 +1,66 @@
+document.getElementById('formBlague').addEventListener('submit', function(e) {
+  e.preventDefault();
+  const categorie = document.getElementById('categorie').value;
+  const nombre = document.getElementById('nombre').value;
+  let nombreFinal;
+  if (nombre != 1) {
+    nombreFinal = '&amount=' + nombre;
+  }
+  else {
+    nombreFinal = '';
+  }
+  let endpoint = `https://v2.jokeapi.dev/joke/${categorie}?lang=fr&blacklistFlags=nsfw,religious,political,racist,sexist,explicit${nombreFinal}`;
+
+  console.log('Catégorie:', categorie);
+  console.log('Nombre:', nombre);
+  console.log('Endpoint généré:', endpoint);
+
+  // 👉 Ton fetch est maintenant dans le submit
+  fetch(endpoint)
+  .then(function (response) {
+      if (response.status == 200) {
+          response.json().then(function (datas) {
+              console.log(datas);
+
+              let blague = document.getElementById('blague');
+              //blague.innerHTML = ""; 
+
+              // plusieurs blagues
+              if (datas.jokes) {
+                  datas.jokes.forEach(function(j) {
+                      blague.appendChild(generateUsersname(j));
+                  });
+              } 
+              // une seule blague
+              else {
+                  blague.appendChild(generateUsersname(datas));
+              }
+          })
+      } else {
+          console.log("Erreur");
+      }
+  })
+})
+
+// Fonction génératrice de lignes
+function generateUsersname(dataUser)
+{
+    let GeneratedUsersname = document.createElement("TR");
+
+    let tdCATEGORIE = document.createElement("TD");
+    let tdBLAGUE = document.createElement("TD");
+    //let tdEDITION = document.createElement("TD");
+
+    tdCATEGORIE.innerHTML = dataUser.category;
+    if (dataUser.type === "single") {
+        tdBLAGUE.innerHTML = dataUser.joke;
+    } else {
+        tdBLAGUE.innerHTML = dataUser.setup + " " + dataUser.delivery;
+    }    //tdEDITION.innerHTML = dataUser.location.city;
+
+    GeneratedUsersname.appendChild(tdCATEGORIE);
+    GeneratedUsersname.appendChild(tdBLAGUE);
+    //GeneratedUsersname.appendChild(tdEDITION);
+
+    return GeneratedUsersname;
+}
